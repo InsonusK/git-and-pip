a1feb6674e63d02049a2b22d8dab03050282a2fc
InsonusK <Insonus.K@gmail.com>
2021-10-30 21:41:33 +0300

    find _packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="It's pip... with git.",
     long_description=long_description,
     url="https://github.com/arocketman/git-and-pip",
-    packages=setuptools.find_packages(where="./gitandpip"),
+    packages=setuptools.find_packages(include=["gitandpip"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
