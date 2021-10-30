4645da569c7821f936b48e2bdb47d2c7a651f9af
InsonusK <insonus.k@gmail.com>
2021-10-30 17:35:11 +0300

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gitandpipins",
-    version="0.0.1",
+    version="0.0.2",
     author="Forker",
     author_email="and.capuano@gmail.com",
     description="It's pip... with git.",
