27882786481a0bc0ce46fe6f1b0f8a0ee4aaf808
InsonusK <Insonus.K@gmail.com>
2021-10-30 23:26:14 +0300

    add test

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setuptools.setup(
     description="It's pip... with git.",
     long_description=long_description,
     url="https://github.com/arocketman/git-and-pip",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=["gitandpip", "gitandpip2", "gitandpip2.*"]),
+    package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
---
+++ src/test/test_gitandpip.py
@@ -0,0 +1,5 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_def_yahoo_call(self) :
+        self.assertEqual(1,1)
